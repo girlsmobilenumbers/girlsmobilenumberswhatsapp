0d4bedcdd0586511389c654a7c5e18c6a788fe2d
girlsmobilenumbers <parthofparth@gmail.com>
2025-09-21 18:52:03 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,1 @@
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
-$servername = "localhost"; // PeekHosting ගෙන් ලබාගත් hostname
-$port = 3306;
-$username = "lionnimesha_pattauser";
-$password = "Dk?&EZC]aNK*";
-$dbname = "lionnimesha_push";
-
-try {
-    $conn = new PDO("mysql:host=$servername;port=$port;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
-} catch(PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
-}
-?>
